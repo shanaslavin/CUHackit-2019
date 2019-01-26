99886098d39ccc0c00e9832d4cf82cab9dcaac90
Shana Slavin <shanaslavin@gmail.com>
2019-01-26 16:35:36 -0500

    Initial commit

---
+++ Hackit/cuhackit/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class dispenser(models.Model):
+    inventory = models.IntegerField()
+    bathroom_name = models.TextField()
+    lat = models.DecimalField(max_digits = 9, decimal_places = 6)
+    lng = models.DecimalField(max_digits = 9, decimal_places = 6)
+
+
+# class logging(models.Model):
---
+++ Hackit/cuhackit/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic import DetailView,TemplateView
+from cuhackit.models import dispenser
+from django.core import serializers
+from django.http import HttpResponse
+
+# Create your views here.
+
+def dispenser_view_single(request, dispenser_id):
+    dispensers = dispenser.objects.filter(pk = dispenser_id)
+    data = serializers.serialize("json", dispensers)
+    response = HttpResponse(data)
+    return response
+
+def dispenser_view(request):
+    if(request.method == "GET"):
+        dispensers = dispenser.objects.all()
+        data = serializers.serialize("json", dispensers)
+        response = HttpResponse(data)
+        return response
+    elif(request.method == "POST"):
+        return HttpResponse()
+    else:
+        return HttpResponse()
+
+class dispenserDetailView(DetailView):
+    model = dispenser
+
+class map(TemplateView):
+    model = dispenser
+    template_name = 'map.html'
